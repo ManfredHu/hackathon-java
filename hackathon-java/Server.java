5b747298eb659e9c8f82715f16cfac5c95321d6e
unknown <beatkirito@gmail.com>
2015-11-27 12:51:19 +0800

    modify app.yml

--- hackathon-java/Server.java
+++ hackathon-java/Server.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.StringBuilder;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -38,14 +39,37 @@ public class Server {
 		public void run() {
 			try {
 				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				String inputLine = in.readLine();
-				String[] contents = inputLine.split(" ");
-				//输出contents
-				System.out.println("TEST!!!!!!!####!@#!@#" + contents);
-				if ("GET".equals(contents[0]) && "/".equals(contents[1])) {
-					socket.getOutputStream().write("Hello, world!test\r\n".getBytes());
-					socket.getOutputStream().flush();
-				}
+
+                String inputLine = null;
+				StringBuilder requestHeader = new StringBuilder();
+                System.out.println("***socket data:  " + Thread.currentThread());
+
+//				inputLine  = in.readLine();
+//				String[] contents = inputLine.split(" ");
+//				if ("GET".equals(contents[0]) && "/".equals(contents[1])) {
+//					socket.getOutputStream().write("Hello, world!\r\n".getBytes());
+//					socket.getOutputStream().flush();
+//				}else{
+//
+//				}
+                while((inputLine  = in.readLine())!= null) {
+
+					System.out.println(Thread.currentThread() + inputLine);
+
+					String[] contents = inputLine.split(" ");
+					if ("GET".equals(contents[0]) && "/".equals(contents[1])) {
+						socket.getOutputStream().write("Hello, world!\r\n".getBytes());
+						socket.getOutputStream().flush();
+					}else{
+
+					}
+                }
+
+				socket.getOutputStream().write("hehe!!\r\n".getBytes());
+				socket.getOutputStream().flush();
+				socket.close();
+
+
 
 			} catch (IOException e) {
 				System.out.println(e.getMessage());
