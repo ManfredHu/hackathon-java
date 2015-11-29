c03852ed0bab7f5cd9f518f55e5e70ed41961a6a
manfred <manfredHuFE@gmail.com>
2015-11-30 00:40:04 +0800

    提交adminOrder

--- hackathon-java/src/com/hackathon/api/AdminOrdersApi.java
+++ hackathon-java/src/com/hackathon/api/AdminOrdersApi.java
@@ -1,5 +1,9 @@
 package com.hackathon.api;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.hackathon.model.Order;
 import com.hackathon.servlet.Request;
 import com.hackathon.servlet.Response;
 import com.hackathon.servlet.Servlet;
@@ -11,7 +15,21 @@ public class AdminOrdersApi extends Servlet {
 
     @Override
     public void doGet(Request request, Response response) {
-
+		//检测是否是后台登陆
+		
+		System.out.println("123456");
+		
+		//获得所有订单的实体
+		List<Order> listOrders = orderDao.getAllOrders();
+		System.out.println("1234568"+listOrders);
+		
+		//输出所有订单
+		try {
+			response.outPut(listOrders);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     @Override
